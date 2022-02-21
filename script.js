4a3c2288d5c166e79dd07b3dd8637319f9813e8d
Joseph <josephgabrieltina@gmail.com>
2022-02-21 16:38:06 +0800

    updating interface

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// default values for testing
 const products = [
     {
         id: 0,
@@ -25,22 +27,43 @@ const products = [
     }
 ]
 
+// onload
 window.addEventListener("DOMContentLoaded", function(){
+
+    // getting numpad elements
     let buttons = document.getElementsByClassName("numpad")
     let code = ""
 
     for(let i = 0; i < buttons.length; i++){
+
+        // click event listener for numpad
         buttons[i].addEventListener("click", function(){
             if(buttons[i].innerHTML == "c"){
+
+                // deleting last character of string
                 code = code.slice(0, code.length-1)
+
+                // resetting fields
+                document.getElementById("name").innerHTML = ""
+                document.getElementById("price").innerHTML = ""
             }else if(buttons[i].innerHTML == "enter"){
 
+                // getting values from list using code as index
+                getProd(code)
             }else{
+
+                // updating code(barcode) as user submits input
                 code = code.concat(buttons[i].innerHTML)
             }
             document.getElementById("code").innerHTML = code
         })
     }
-
     console.log(buttons)
 })
+
+function getProd(code){
+
+    // updating interface for product name and price
+    document.getElementById("name").innerHTML = products[code].name
+    document.getElementById("price").innerHTML = "$ " + products[code].price
+}
