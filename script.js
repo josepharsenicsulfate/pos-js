bb6876db4c74eb19508a8ee3bccb531bd42981c0
Joseph <josephgabrieltina@gmail.com>
2022-02-22 10:42:55 +0800

    added styling and key listener functionality

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
                 // getting values from list using code as index
                 getProd(code)
-            }else{
+            }else if(isDigit(buttons[i].innerHTML)){
 
                 // updating code(barcode) as user submits input
                 code = code.concat(buttons[i].innerHTML)
@@ -57,17 +57,67 @@ window.addEventListener("DOMContentLoaded", function(){
             document.getElementById("code").innerHTML = code
         })
     }
+
+    document.addEventListener("keydown", (event) => {
+        const keyName = event.key
+        if(isKey(keyName)){
+            if(isDigit(keyName)){
+                code = code.concat(keyName)
+            }else if(keyName == "Backspace"){
+                code = code.slice(0, code.length-1)
+            }else if(keyName == "Enter"){
+                getProd(code)
+            }
+        }
+
+        document.getElementById("code").innerHTML = code
+    }, false)
 })
-
-window.addEventListener("keydown", (event) => {
-    const keyName = event.key
-
-    console.log(keyName)
-}, false)
 
 function getProd(code){
 
     // updating interface for product name and price
     document.getElementById("name").innerHTML = products[code].name
     document.getElementById("price").innerHTML = "$ " + products[code].price
+}
+
+function isDigit(char){
+
+    // identifying if keyboard input is digit
+    if(char == 0 
+        || char == 1 
+        || char == 2 
+        || char == 3 
+        || char == 4 
+        || char == 5 
+        || char == 6 
+        || char == 7 
+        || char == 8 
+        || char == 9
+        ){
+            return true
+        }
+}
+
+function isControl(char){
+
+    // identifying if keyboard input is control in keypad
+    if(char == "."
+        || char == "Backspace"
+        || char == "ArrowUp"
+        || char == "ArrowDown"
+        || char == "Enter"
+        ){
+            return true
+        }
+}
+
+function isKey(char){
+
+    // identifying if keyboard input is in keypad
+    if(isDigit(char)){
+        return true
+    }else if(isControl(char)){
+        return true
+    }
 }
